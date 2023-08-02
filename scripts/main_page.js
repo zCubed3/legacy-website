4ce87228ebf457afdfc9a3b4feecfd720fe6d9a3
zCubed3 <zcubedtres@gmail.com>
2023-08-02 11:41:56 -0700

    Generative page design!

---
+++ scripts/main_page.js
@@ -0,0 +1,102 @@
+let plt_major_card = null
+let plt_row = null
+
+let pl_major_container = null
+
+let pl_cur_row = null
+let pl_cur_row_step = 0
+
+function verifyTemplates() {
+    const parser = new DOMParser()
+
+    if (plt_major_card == null) {
+        fetch("/prefabs/plt_major_card.html")
+            .then(response => response.text())
+            .then(function(contents) {
+                plt_major_card = parser.parseFromString(contents, "text/html")
+            })
+    }
+
+    // TODO: Does this need to be hard coded?
+    plt_row = parser.parseFromString('<div class="flex_row"></div>>', "text/html")
+}
+
+function createCards(response, major) {
+    let base = response["get_base"]
+
+    response["schemas"].forEach(function(path) {
+        let concat = base + path
+
+        fetch(concat)
+            .then(card => card.json())
+            .then(function(card) {
+                console.log(card)
+
+                // Duplicate the document
+                let root = plt_major_card.getElementById("prefab_root")
+
+                let name = card["name"]
+
+                if (card["name_is_link"] === "true") {
+                    let elem_a = document.createElement("a")
+                    elem_a.href = card["link"]
+                    elem_a.innerText = name
+
+                    plt_major_card.getElementById("card_title").innerHTML = elem_a.outerHTML
+                } else {
+                    plt_major_card.getElementById("card_title").innerHTML = name
+                }
+
+                plt_major_card.getElementById("card_desc").innerHTML = card["description"]
+
+                plt_major_card.getElementById("card_date").innerHTML = card["date"]
+                plt_major_card.getElementById("card_lang").innerHTML = card["lang"]
+                plt_major_card.getElementById("card_state").innerHTML = card["state"]
+
+                let dupe = root.cloneNode(true)
+
+                // Have we stepped too far?
+                if (major && pl_cur_row_step >= 2) {
+                    pl_cur_row_step = 0;
+                    pl_cur_row = null;
+                }
+
+                // Do we have a row yet (major only)?
+                if (major && pl_cur_row == null) {
+                    pl_cur_row = document.createElement("div")
+                    pl_cur_row.classList.add("flex_row")
+
+                    pl_major_container.append(pl_cur_row)
+                }
+
+                if (major) {
+                    pl_cur_row.appendChild(dupe)
+                    pl_cur_row_step += 1
+                } else {
+                    alert("Please report this, the minor PML should NOT have been loaded yet!")
+                }
+
+                console.log(dupe)
+            })
+    })
+}
+
+
+function main() {
+    verifyTemplates()
+
+    pl_major_container = document.getElementById("pl_major_container")
+
+    // Load our Major and Minor PML files
+    fetch("/projects/pml_major.json")
+        .then(response => response.json())
+        .then(function(response) {
+            createCards(response, true)
+        })
+
+    // Destroy HTML warnings
+    document.getElementById("pl_major_warning").remove()
+    console.log("Did the warning go away?")
+}
+
+window.onload = main
